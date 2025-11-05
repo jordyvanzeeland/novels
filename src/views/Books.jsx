18acf2a6c67efbe289ee1dc9c99252d5c4a2f409
Jordy van Zeeland <jordyvanzeeland@gmail.com>
2025-11-05 09:03:47 +0100

    Fintuning first version

--- src/views/Books.jsx
+++ src/views/Books.jsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
 import moment from 'moment';
-import withAuth from "../components/WithAuth";
 import { fetchApi } from "../Functions";
-import AddBookModal from "../components/AddBookModal";
 moment.locale('nl');
 
 const Books = () => {
     const currentyear = new Date().getFullYear();
     const [year, setYear] = useState(currentyear);
     const [readingYears, setReadingYears] = useState([]);
+    const [showModal, setShowModal] = useState(false);
     const [books, setBooks] = useState([]);
 
     const getData = async () => {
@@ -19,6 +18,26 @@ const Books = () => {
         setBooks(getBooksOfYear);
     }
 
+    const addNewBook = async (event) => {
+        event.preventDefault();
+
+        const bookname = event.target.book.value;
+        const author = event.target.author.value;
+        const genre = event.target.genre.value;
+        const readedMonth = event.target.readed.value;
+        const rating = event.target.rating.value;
+        const language = event.target.language.value;
+
+        const readedDate = `${year}-${readedMonth}-01`;
+
+        await fetchApi('POST', `books/insert`, {year: year}, {
+            name: bookname, author: author, genre: genre, readed: readedDate, rating: rating, en: language
+        });
+
+        setShowModal(false);
+        await getData();
+    }
+
     useEffect(() => {
         getData();
       }, [year]);
@@ -26,6 +45,8 @@ const Books = () => {
     return (
         <React.Fragment>
             <div className='header'>
+                <div className="btn btn-new" onClick={() => setShowModal(true)}><i className="fa-solid fa-plus"></i> Niew boek</div>
+                
                 <div className="logo">
                     <img src="/logo-white.png" />
                 </div>
@@ -44,10 +65,10 @@ const Books = () => {
             </div>
     
             <div className="container">
-                <div class="bookslist">
+                <div className="bookslist">
                     <div className="row">
                         <div className="col-md-12">
-                            <table id="DataTable" class="display" width="100%">
+                            <table id="DataTable" className="display" width="100%">
                                 <thead>
                                     <tr>
                                         <th>Boek</th>
@@ -58,7 +79,7 @@ const Books = () => {
                                         <th></th>
                                     </tr>
                                 </thead>
-                                <tbody class="table-content">
+                                <tbody className="table-content">
                                     {books.map(book => {
                                         return (
                                             <tr key={book.id}>
@@ -66,12 +87,12 @@ const Books = () => {
                                                     <div style={{ color: "#333", fontWeight: "400" }}>{book.name}</div>
                                                     <div style={{ color: "#666" }}>{book.author}</div>
                                                     <div className="genre-mobile">
-                                                        <div class="catColor" style={{ background: book.genre === 'Thriller' ? "rgb(62,69,113)" : (book.genre === "Roman" ? "rgb(16, 115, 95)" : (book.genre === "Non-fictie" ? "rgb(220,116,105)" : "rgb(146,48,67)")) }}>{book.genre}</div>
+                                                        <div className="catColor" style={{ background: book.genre === 'Thriller' ? "rgb(62,69,113)" : (book.genre === "Roman" ? "rgb(16, 115, 95)" : (book.genre === "Non-fictie" ? "rgb(220,116,105)" : "rgb(146,48,67)")) }}>{book.genre}</div>
                                                     </div>
                                                 </td>
                                                 <td className="genre-desktop"><div className="catColor" style={{ background: book.genre === 'Thriller' ? "rgb(62,69,113)" : (book.genre === "Roman" ? "rgb(16, 115, 95)" : (book.genre === "Non-fictie" ? "rgb(220,116,105)" : "rgb(146,48,67)")) }}>{book.genre}</div></td>
                                                 <td>{moment(book.readed).format("MMMM")}</td>
-                                                <td class="rating"><i className="fa-solid fa-star text-yellow-500"></i>{book.rating}</td>
+                                                <td className="rating"><i className="fa-solid fa-star text-yellow-500"></i>{book.rating}</td>
                                             </tr>
                                         )
                                     })}
@@ -81,10 +102,77 @@ const Books = () => {
                     </div>
                 </div>
 
-                <AddBookModal year={year}/>
+                {showModal && (
+                    <div className="modal" style={{ display: "block" }} tabindex="-1">
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h3 className="modal-title">Boek toevoegen</h3>
+                                    <button type="button" onClick={() => setShowModal(false)} className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div className="modal-body">
+                                    <form method="POST" id="addbook" onSubmit={(event) => addNewBook(event)}>
+                                        <div className="mb-3">
+                                            <label for="book" className="form-label">Boek</label>
+                                            <input type="text" className="form-control" name="book" id="book" required/>
+                                        </div>
+                                        <div className="mb-3">
+                                            <label for="author" className="form-label">Schrijver</label>
+                                            <input type="text" className="form-control" name="author" id="author" required/>
+                                        </div>
+                                        <div className="mb-3">
+                                            <label for="genre" className="form-label">Genre</label>
+                                            <select className="form-control" name="genre" id="genre" required>
+                                                <option value="Thriller">Thriller</option>
+                                                <option value="Roman">Roman</option>
+                                                <option value="Non-fictie">Non-fictie</option>
+                                                <option value="Young Adult">Young Adult</option>
+                                            </select>
+                                        </div>
+                                        <div className="mb-3">
+                                            <label for="readed" className="form-label">Maand</label>
+                                            <select className="form-control" name="readed" id="readed" required>
+                                                <option value="01">Januari</option>
+                                                <option value="02">Februari</option>
+                                                <option value="03">Maart</option>
+                                                <option value="04">April</option>
+                                                <option value="05">Mei</option>
+                                                <option value="06">Juni</option>
+                                                <option value="07">Juli</option>
+                                                <option value="08">Augustus</option>
+                                                <option value="09">September</option>
+                                                <option value="10">Oktober</option>
+                                                <option value="11">November</option>
+                                                <option value="12">December</option>
+                                            </select>
+                                        </div>
+                                        <div className="mb-3">
+                                            <label for="rating" className="form-label">Aantal sterren</label>
+                                            <select className="form-control" name="rating" id="rating" required>
+                                                <option value="1">1</option>
+                                                <option value="2">2</option>
+                                                <option value="3">3</option>
+                                                <option value="4">4</option>
+                                                <option value="5">5</option>
+                                            </select>
+                                        </div>
+                                        <div className="mb-3">
+                                            <label for="en" className="form-label">Taal</label>
+                                            <select className="form-control" name="language" id="language" required>
+                                                <option value="0">Nederlands</option>
+                                                <option value="1">English</option>
+                                            </select>
+                                        </div>
+                                        <button type="submit" className="btn btn-green">Toevoegen</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
         </React.Fragment>
     )
 }
 
-export default withAuth(Books)
+export default Books;
